528c1d01e6d671ebbe53ace84d1181040f436a14
SodiumZar <nazarya2005@gmail.com>
2025-11-27 05:19:01 +0800

    feat: create Layout wrapper component

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,1 +1,23 @@
 // Layout Wrapper Component
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      {/* Navbar at top */}
+      <Navbar />
+      
+      {/* Main content area - grows to fill space */}
+      <main className="grow">
+        <Outlet />
+      </main>
+      
+      {/* Footer at bottom */}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
